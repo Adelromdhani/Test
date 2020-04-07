7af52d36c7c9c63a28b305ea5b7b96e9807a7e0d
Adel Romdhani <adel.romdhani@esprit.tn>
2020-04-07 20:45:38 +0200

    test

---
+++ src/main/java/tn/esprit/Hello/HelloMain.java
@@ -0,0 +1,13 @@
+package tn.esprit.Hello;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hello 3");
+		System.out.println("last test 2");
+		System.out.println("test 3");
+	}
+
+}
